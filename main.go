4ed646b2c0706c493ddf4734afb7fda090632157
aris kusnul widitama <ariskusnul@gmail.com>
2022-03-20 16:39:01 +0700

    test validation token

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+
 	"urunan/auth"
 	"urunan/handler"
 	"urunan/user"
@@ -27,16 +27,7 @@ func main() {
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 	
-	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.JnTpSCBf41jzuELlysZx6pJODT5G4gIK4VNBVqyos54")
-	if err != nil {
-		fmt.Println("Error")
-	}
-	if token.Valid {
-		fmt.Println("token valid")
-	}else{
-		fmt.Println("token invalid")
-	}
-
+	
 	userHandler := handler.NewUserHandler(userService, authService)
 
 
